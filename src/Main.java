24eab7967472cb61c8de8d177db499ae2cc70473
liurenrui <liurenrui@xiaohongshu.com>
2024-08-17 15:18:58 +0800

    加了for

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("test");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+
     }
 }
